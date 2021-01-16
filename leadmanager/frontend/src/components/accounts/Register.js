6c643a845b38c0679962dc4f4b7c93da6babe805
Rohit Dubey <dubey22rohit@gmail.com>
2021-01-17 00:08:58 +0530

    Authentication Bugs Fixed

--- leadmanager/frontend/src/components/accounts/Register.js
+++ leadmanager/frontend/src/components/accounts/Register.js
@@ -1,44 +1,55 @@
-import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import { register } from "../../actions/auth";
-import { createMessage } from "../../actions/messages";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { register } from '../../actions/auth';
+import { createMessage } from '../../actions/messages';
 
 export class Register extends Component {
   state = {
-    username: "",
-    email: "",
-    password: "",
-    confpassword: "",
+    username: '',
+    email: '',
+    password: '',
+    password2: '',
   };
+
+  static propTypes = {
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+  };
+
   onSubmit = (e) => {
     e.preventDefault();
-    const { username, email, password, confpassword } = this.setState;
-    if (password != confpassword) {
-      this.props.createMessage({ passwordNotMatch: "Passwords do not match!" });
+    const { username, email, password, password2 } = this.state;
+    if (password !== password2) {
+      this.props.createMessage({ passwordNotMatch: 'Passwords do not match' });
     } else {
-      const newUser = { username, email, password };
+      const newUser = {
+        username,
+        password,
+        email,
+      };
       this.props.register(newUser);
     }
   };
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     if (this.props.isAuthenticated) {
       return <Redirect to="/" />;
     }
-    const { username, email, password, confpassword } = this.state;
+    const { username, email, password, password2 } = this.state;
     return (
-      <div>
-        <div className="card card-body mt-6 mb-4">
+      <div className="col-md-6 m-auto">
+        <div className="card card-body mt-5">
+          <h2 className="text-center">Register</h2>
           <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label>Username</label>
               <input
-                className="form-control"
                 type="text"
+                className="form-control"
                 name="username"
                 onChange={this.onChange}
                 value={username}
@@ -47,8 +58,8 @@ export class Register extends Component {
             <div className="form-group">
               <label>Email</label>
               <input
+                type="email"
                 className="form-control"
-                type="text"
                 name="email"
                 onChange={this.onChange}
                 value={email}
@@ -57,8 +68,8 @@ export class Register extends Component {
             <div className="form-group">
               <label>Password</label>
               <input
+                type="password"
                 className="form-control"
-                type="text"
                 name="password"
                 onChange={this.onChange}
                 value={password}
@@ -67,11 +78,11 @@ export class Register extends Component {
             <div className="form-group">
               <label>Confirm Password</label>
               <input
+                type="password"
                 className="form-control"
-                type="text"
-                name="confpassword"
+                name="password2"
                 onChange={this.onChange}
-                value={confpassword}
+                value={password2}
               />
             </div>
             <div className="form-group">
@@ -80,7 +91,7 @@ export class Register extends Component {
               </button>
             </div>
             <p>
-              Already have an account?<Link to="/login">Login</Link>
+              Already have an account? <Link to="/login">Login</Link>
             </p>
           </form>
         </div>
@@ -92,4 +103,5 @@ export class Register extends Component {
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
 });
+
 export default connect(mapStateToProps, { register, createMessage })(Register);
